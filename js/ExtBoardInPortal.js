fcbbf91d633bc534c02f778711382deb15630f05
Koh <gpc2843661009@gmail.com>
2023-05-16 11:47:21 +0800

    Update ExtBoardInPortal.js
    - Change behavior when hovering
    on buttons.
    - Add support for zh-Hant.

--- js/ExtBoardInPortal.js
+++ js/ExtBoardInPortal.js
@@ -11,18 +11,28 @@ a#n-sidebar-discussionboard-new {
 	color: #0D77AC !important;
 	font-weight: bold;
 	text-shadow: none;
-	transition: all .1s ease-in;
+	transition: background .1s ease-in;
 	width: 15px;
 	height: 15px;
+	overflow: hidden;
+	text-overflow: clip;
+	white-space: nowrap;
 }
 a#n-sidebar-discussionboard-new:hover {
 	background: #1AA7EE;
 	color: #FFF !important;
 	text-decoration: none;
+	width: 4.7em;
+}
+#n-sidebar-discussionboard-new:hover::before {
+	content: "${wgULS('添加话题', '添加話題')}";
+	display: inline-block;
+	font-size: .8em;
+	margin-right: .5em;
 }
 `);
 var discussionBoard = $('#n-sidebar-discussionboard').clone();
-var discussionBoardNames = ['技术实现', '权限变更', '操作申请', '方针政策', '页面相关', '提问求助', '群组信息'];
+var discussionBoardNames = [wgULS('技术实现', '技術實現'), wgULS('权限变更', '權限變更'), wgULS('操作申请', '操作申請'), wgULS('方针政策', '方針政策'), wgULS('页面相关', '頁面相關'), wgULS('提问求助', '提問求助'), wgULS('群组信息', '群組資訊')];
 var discussionBoardAbbrs = ['tec', 'rgt', 'act', 'pol', 'pge', 'que', 'grp'];
 var discussionBoardPortalObj = $('<div>').attr({
 		'id': 'p-discussionboard',
@@ -30,7 +40,7 @@ var discussionBoardPortalObj = $('<div>').attr({
 		'role': 'navigation',
 		'aria-labelledby': 'p-discussionboard-label'
 	})
-	.append($('<h3>').attr('id', 'p-discussionboard-label').text('讨论版'))
+	.append($('<h3>').attr('id', 'p-discussionboard-label').text(wgULS('讨论版', '討論版')))
 	.append($('<div>').addClass('body').append($('<ul>')));
 $('#p-navigation').after(discussionBoardPortalObj);
 $('#n-sidebar-discussionboard').hide();
@@ -39,8 +49,7 @@ for (var i = 0; i < discussionBoardNames.length; i++) {
 		.attr({
 			'href': '/index.php?title=萌娘百科_talk:讨论版/' + discussionBoardNames[i] + '&action=edit&section=new',
 			'target': '_blank',
-			'id': 'n-sidebar-discussionboard-new',
-			'title': '在' + discussionBoardNames[i] + '版下添加话题'
+			'id': 'n-sidebar-discussionboard-new'
 		});
 	var discussionBoardLiObj = $('<li>').attr('id', 'n-sidebar-discussionboard' + discussionBoardAbbrs[i])
 		.append($('<a>').attr('href', '/萌娘百科_talk:讨论版/' + discussionBoardNames[i]).text(discussionBoardNames[i]))
