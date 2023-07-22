ad7b843511b6aef7797fc29fc9364e6655fcdba9
Koh <gpc2843661009@gmail.com>
2023-07-22 20:46:59 +0800

    Update BgInfo.js

--- js/BgInfo.js
+++ js/BgInfo.js
@@ -1,3 +1,5 @@
+/* Icon path originated from Remix Icon */
+
 $(() => (async () => {
 	await mw.loader.using("ext.gadget.SideBarPic");
 	if ($('.sidebar-character > img').length > 0 && $('.sidebar-character > img').is(':visible') && $('.sidebar-character').attr('data-displaylogo')) {
@@ -20,7 +22,7 @@ $(() => (async () => {
 		.pc-bg-info-icon {
 			display: inline-block;
 			background: #1A2947;
-			clip-path: path("M12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2C17.5228 2 22 6.47715 22 12C22 17.5228 17.5228 22 12 22ZM12 20C16.4183 20 20 16.4183 20 12C20 7.58172 16.4183 4 12 4C7.58172 4 4 7.58172 4 12C4 16.4183 7.58172 20 12 20ZM11 7H13V9H11V7ZM11 11H13V17H11V11Z");
+			clip-path: path("M20 5H4V19L13.2923 9.70649C13.6828 9.31595 14.3159 9.31591 14.7065 9.70641L20 15.0104V5ZM2 3.9934C2 3.44476 2.45531 3 2.9918 3H21.0082C21.556 3 22 3.44495 22 3.9934V20.0066C22 20.5552 21.5447 21 21.0082 21H2.9918C2.44405 21 2 20.5551 2 20.0066V3.9934ZM8 11C6.89543 11 6 10.1046 6 9C6 7.89543 6.89543 7 8 7C9.10457 7 10 7.89543 10 9C10 10.1046 9.10457 11 8 11Z");
 			width: 24px;
 			height: 24px;
 			transform: scale(.6);
