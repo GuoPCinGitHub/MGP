8d4b2d5f18cf2fccf0475c8b1ef109528c777270
Koh <gpc2843661009@gmail.com>
2023-08-16 12:09:17 +0800

    Update WatchlistNoRed.js

--- js/WatchlistNoRed.js
+++ js/WatchlistNoRed.js
@@ -1,25 +1,25 @@
 $(function () {
-	if (mw.config.get('wgCanonicalSpecialPageName') == 'EditWatchlist' && $('.mw-htmlform').length > 0) {
-		$('#mw-content-text').prepend('<input type="button" value="移除红链（不包括提案页面）" style="margin: 5px 0;" onclick="removeRedLinks()">');
+	if (mw.config.get('wgTitle') == '编辑监视列表' && $('.mw-htmlform').length > 0) {
+		$('#mw-content-text').prepend('<input type="button" value="' + wgULS('移除红链（不包括提案页面）', '移除紅連（不包括提案頁面）') +'" style="margin: 5px 0;" onclick="removeRedLinks()">');
 	}
 });
 
 function removeRedLinks() {
-	var selectedNum = 0;
+	let selectedNum = 0;
 	$('.mw-htmlform-flatlist-item').each(function () {
-		var title = $(this).children('label').children(':first');
+		const title = $(this).children('label').children(':first');
 		if (title.attr('class') == 'new' && title.text().substr(0, 8) != '萌娘百科:提案/') {
 			$(this).children('input').attr('checked', true);
 			selectedNum += 1;
 		}
 	});
 	if (selectedNum !== 0) {
-		mw.notify('已选中 ' + selectedNum + ' 个标题，即将移除……', {autoHide: true, autoHideSeconds: 10, tag: 'SRL', title: '移除红链', type: 'warn'});
+		mw.notify(wgULS('已选中', '已選中') + ' ' + selectedNum + ' ' + wgULS('个标题，即将移除……', '個標題，即將移除……'), {autoHide: true, autoHideSeconds: 10, tag: 'SRL', title: wgULS('移除红链', '移除紅連'), type: 'warn'});
 		let notifyTimeout = setTimeout(() => {
 			$('input.mw-htmlform-submit').trigger('click');
 			clearTimeout(notifyTimeout);
 		}, 5000);
 	} else {
-		mw.notify('列表中无红链！溜了溜了……', {autoHide: true, autoHideSeconds: 10, tag: 'SRL', title: '移除红链', type: 'error'});
+		mw.notify(wgULS('列表中无红链！溜了溜了……', '列表中無紅連！溜了溜了……'), {autoHide: true, autoHideSeconds: 10, tag: 'SRL', title: wgULS('移除红链', '移除紅連'), type: 'error'});
 	}
 }
