a46e3220b6bb36b66542e8a92348d8b205d3c3fc
Saki Itomi <gpc2843661009@gmail.com>
2024-01-06 21:04:24 +0800

    Update 4 files
    - Adjust font fallback (common.css L23).
    - Specify link color for copying permanent links for sections (common.css L348).
    - Clean codes no longer in use (common.js).
    - Hide VIP-related contents (moeskin.css L36).
    - Add icons for page tools (moeskin.css L55).
    - Simply adjust colors and add icons for edit buttons in mobile view (moeskin.css L108).
    - Add entries to user link area (moeskin.js L1).

--- moeskin.js
+++ moeskin.js
@@ -1,8 +1,15 @@
-// 向自定义工具中加链
-mw.util.addPortletLink('p-personal', '/分类:积压工作', '积压工作', 'pt-overstock', '归类为积压工作的内容[Alt+Shift+o]', 'o');
-mw.util.addPortletLink('p-personal', '/分类:萌娘百科数据报告', '数据报告', 'pt-datareport', '展示统计报告[Alt+Shift+d]', 'd');
-mw.util.addPortletLink('p-personal', '/User:' + mw.config.get("wgUserName") + '/Sandbox', '我的沙盒', 'pt-mysandbox', '您的个人沙盒页面[Alt+Shift+s]', 's');
-mw.util.addPortletLink('p-personal', '/Special:log/' + mw.config.get("wgUserName"), '我的日志', 'pt-mylog', '您执行的所有公开日志[Alt+Shift+g]', 'g');
+// Add entries to user link area
+var texts = ['沙盒', '积压', '数据'];
+var hrefs = ['/User:GuoPC/Sandbox', '/Category:积压工作', '/Category:萌娘百科数据报告'];
+var names = ['tabler-icon-sandbox', 'tabler-icon-file-stack', 'tabler-icon-report-analysis'];
+var paths = ['<path d="M19.953 8.017l1.047 6.983v2a3 3 0 0 1 -3 3h-12a3 3 0 0 1 -3 -3v-2l1.245 -8.297a2 2 0 0 1 1.977 -1.703h3.778" /><path d="M3 15h18" /><path d="M13 3l5.5 1.5" /><path d="M15.75 3.75l-2 7" /><path d="M7 10.5c1.667 -.667 3.333 -.667 5 0c1.667 .667 3.333 .667 5 0" />', '<path d="M14 3v4a1 1 0 0 0 1 1h4" /><path d="M5 12v-7a2 2 0 0 1 2 -2h7l5 5v4" /><path d="M5 21h14" /><path d="M5 18h14" /><path d="M5 15h14" />', '<path d="M9 5h-2a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-12a2 2 0 0 0 -2 -2h-2" /><path d="M9 3m0 2a2 2 0 0 1 2 -2h2a2 2 0 0 1 2 2v0a2 2 0 0 1 -2 2h-2a2 2 0 0 1 -2 -2z" /><path d="M9 17v-5" /><path d="M12 17v-1" /><path d="M15 17v-3" />'];
+
+for (var i = 0; i < texts.length; i++) {
+	var nulg = $('.nav-user-link-group:last').clone();
+	nulg.children('div').children('a').attr('href', hrefs[i]).children().children().toggleClass('tabler-icon-clock-edit ' + names[i]).html(paths[i]);
+	nulg.children('a').attr('href', hrefs[i]).text(texts[i]);
+	nulg.appendTo('.user-links');
+}
 
 // Mainpage
 $('.site-logo-area > a').attr('href', '/User:GuoPC/M');
