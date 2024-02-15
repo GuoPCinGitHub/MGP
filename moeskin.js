4e9060d9ef8febf77023a49614c8082499113e12
Koh <gpc2843661009@gmail.com>
2024-02-15 15:33:24 +0800

    Update 2 files
    - Clean and adjust for hidden elements (moeskin.css L37).
    - Adjust cloned element (moeskin.js L8).

--- moeskin.js
+++ moeskin.js
@@ -5,7 +5,7 @@ var names = ['tabler-icon-sandbox', 'tabler-icon-file-stack', 'tabler-icon-repor
 var paths = ['<path d="M19.953 8.017l1.047 6.983v2a3 3 0 0 1 -3 3h-12a3 3 0 0 1 -3 -3v-2l1.245 -8.297a2 2 0 0 1 1.977 -1.703h3.778" /><path d="M3 15h18" /><path d="M13 3l5.5 1.5" /><path d="M15.75 3.75l-2 7" /><path d="M7 10.5c1.667 -.667 3.333 -.667 5 0c1.667 .667 3.333 .667 5 0" />', '<path d="M14 3v4a1 1 0 0 0 1 1h4" /><path d="M5 12v-7a2 2 0 0 1 2 -2h7l5 5v4" /><path d="M5 21h14" /><path d="M5 18h14" /><path d="M5 15h14" />', '<path d="M9 5h-2a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-12a2 2 0 0 0 -2 -2h-2" /><path d="M9 3m0 2a2 2 0 0 1 2 -2h2a2 2 0 0 1 2 2v0a2 2 0 0 1 -2 2h-2a2 2 0 0 1 -2 -2z" /><path d="M9 17v-5" /><path d="M12 17v-1" /><path d="M15 17v-3" />'];
 
 for (var i = 0; i < texts.length; i++) {
-	var nulg = $('.nav-user-link-group:last').clone();
+	var nulg = $('.nav-user-link:last').clone();
 	nulg.children('div').children('a').attr('href', hrefs[i]).children().children().toggleClass('tabler-icon-clock-edit ' + names[i]).html(paths[i]);
 	nulg.children('a').attr('href', hrefs[i]).text(texts[i]);
 	nulg.appendTo('.user-links');
