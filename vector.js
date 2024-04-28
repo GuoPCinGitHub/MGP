dc0721c56b4d52df124dde278dd40151bc93cd63
Koh <gpc2843661009@gmail.com>
2024-04-28 15:58:21 +0800

    Update 3 files
    - Remove shadows of InPageEdit toolbox (common.css L368).
    - Reset font weight for Template:剧透提醒隐藏 (common.css L571).
    - Adjust float position of notifications (vector.css L52).
    - Insert indicators for moderation notices (vector.js L17).

--- vector.js
+++ vector.js
@@ -14,6 +14,39 @@ if (mw.config.get('wgNamespaceNumber') != -1) {
 	$('#right-navigation .menu ul').append('<li id="pc-purge"><a href="/index.php?title=' + mw.config.get('wgPageName') + '&action=purge" style="color: #D4A039;">清除缓存</a></li>');
 }
 
+// Mod
+if (mw.config.get("wgNamespaceNumber") != -1
+	&& !["submit", "edit"].includes(mw.config.get("wgAction"))
+	&& $('.moderation-notice').length > 0) {
+	var setModIcon = function (title, d) {
+		return '<span class="pc-mod-icon" title="' + title + '" style="display:inline-flex;align-items:center;justify-content:center;width:21px;height:31px;margin-right:.5em;padding:0 5px;vertical-align:middle;"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#800040"><path d="' + d + '"></path></svg></span>';
+	};
+	var modText = $('.moderation-notice').text();
+	var modSituList = {
+		'self': '自己的编辑',
+		'skpd': '被跳过',
+		'prev': '不是页面的最新版本'
+	};
+	var modNameList = {
+		'self': '可查看自己当前的编辑但等待审核',
+		'skpd': '较新版本通过审核而被跳过',
+		'prev': '当前阅读的不是最新版本'
+	};
+	var modPathList = {
+		'self': 'M16 2L21 7V21.0082C21 21.556 20.5551 22 20.0066 22H3.9934C3.44476 22 3 21.5447 3 21.0082V2.9918C3 2.44405 3.44495 2 3.9934 2H16ZM12 11.5C13.3807 11.5 14.5 10.3807 14.5 9C14.5 7.61929 13.3807 6.5 12 6.5C10.6193 6.5 9.5 7.61929 9.5 9C9.5 10.3807 10.6193 11.5 12 11.5ZM7.52746 17H16.4725C16.2238 14.75 14.3163 13 12 13C9.68372 13 7.77619 14.75 7.52746 17Z',
+		'skpd': 'M16 2L21 7V21.0082C21 21.556 20.5551 22 20.0066 22H3.9934C3.44476 22 3 21.5447 3 21.0082V2.9918C3 2.44405 3.44495 2 3.9934 2H16ZM15 11V10C15 8.34315 13.6569 7 12 7C10.3431 7 9 8.34315 9 10V11H8V16H16V11H15ZM13 11H11V10C11 9.44772 11.4477 9 12 9C12.5523 9 13 9.44772 13 10V11Z',
+		'prev': 'M16 2L21 6.999V21.0082C21 21.556 20.5551 22 20.0066 22H3.9934C3.44476 22 3 21.5447 3 21.0082V2.9918C3 2.44405 3.44495 2 3.9934 2H16ZM13 9H11V15H16V13H13V9Z'
+	};
+	$.each(modSituList, function (k, v) {
+		var modSituReg = new RegExp(v);
+		if (modSituReg.test(modText)) {
+			$('.mw-indicators').append(setModIcon(modNameList[k], modPathList[k]));
+			return false;
+		}
+	});
+	$('#ca-edit a').attr('href', $('#ca-edit a').attr('href').replace(/&oldid=\d+/g, ''));
+}
+
 // Inspector小工具自定义，见[[User:Bhsd/Inspector#偏好设置]]
 window.inspector = {
 	summary: '// Edit via Inspector',
