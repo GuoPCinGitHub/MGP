4b9881d27f95572029ec38f027b366b00adfdb80
Koh <gpc2843661009@gmail.com>
2025-07-31 18:58:45 +0800

    Update AccesskeyVec.js & ExtBoardInPortal.js
    - Adapt to Vector-2022 skin.

--- js/AccesskeyVec.js
+++ js/AccesskeyVec.js
@@ -3,9 +3,9 @@ mw.loader.addStyleTag(`
 	content: attr(accesskey);
 	display: block;
 	position: absolute;
-	top: 5px;
+	top: 50%;
 	right: 5px;
-	background: rgba(255, 255, 255, 0.8);
+	background: var(--background-color-interactive, rgba(255, 255, 255, 0.8));
 	border-radius: 2px;
 	color: #222;
 	font-family: monospace;
@@ -13,6 +13,7 @@ mw.loader.addStyleTag(`
 	font-weight: bold;
 	text-shadow: none;
 	text-transform: capitalize;
+	transform: translateY(-50%);
 	padding: 0px 2px;
 	user-select: none;
 }
@@ -20,7 +21,7 @@ mw.loader.addStyleTag(`
 .personal-key::after {
 	content: attr(accesskey);
 	display: inline-block;
-	background: rgba(255, 255, 255, 0.8);
+	background: var(--background-color-interactive, rgba(255, 255, 255, 0.8));
 	border-radius: 2px;
 	color: #222;
 	font-family: monospace;
@@ -30,20 +31,34 @@ mw.loader.addStyleTag(`
 	margin: 0px 1px;
 	padding: 0px 2px;
 	user-select: none;
-	vertical-align: sub;
+	vertical-align: ${mw.config.get('skin') == 'vector-2022' ? 'middle' : 'sub'};
+}
+
+.sticky-key::after {
+	content: attr(thekey) !important;
 }
 `);
 
 $(function() {
-	if ($('.portal li').length > 0 && $('#p-personal li').length > 0) {
-		$('.portal li').each(function() {
+	const NEW_SKIN = mw.config.get('skin') == 'vector-2022' ? true : false;
+	const PORTAL_ELE = NEW_SKIN ? '#vector-main-menu li' : '.portal li';
+	const PERSONAL_ELE = NEW_SKIN ? '.vector-user-links-main li' : '#p-personal li';
+	const STICKY_ELE = NEW_SKIN ? '.vector-sticky-header-icons a' : 'body';
+	if ($(PORTAL_ELE).length > 0 && $(PERSONAL_ELE).length > 0 && $(STICKY_ELE).length > 0) {
+		$(PORTAL_ELE).each(function() {
 			if ($(this).children('a').attr('accesskey')) {
 				$(this).css('position', 'relative');
 				$(this).children('a').addClass('portal-key');
 			}
 		});
-		$('#p-personal li').each(function() {
-			if ($(this).children('a').attr('accesskey')) { $(this).children('a').addClass('personal-key'); }
+		$(PERSONAL_ELE).each(function() {
+			if ($(this).children('a').attr('accesskey')) { $(this).children('a').addClass('personal-key');}
+		});
+		$(STICKY_ELE).each(function() {
+			if (/\[*\]/g.test($(this).attr('title'))) {
+				$(this).attr('thekey', $(this).attr('title').substr(-2, 1));
+				$(this).addClass('personal-key sticky-key');
+			}
 		});
 	}
 });
--- js/ExtBoardInPortal.js
+++ js/ExtBoardInPortal.js
@@ -1,8 +1,9 @@
 mw.loader.addStyleTag(`
 a#n-sidebar-discussionboard-new {
 	position: absolute;
-	top: 4px;
+	top: 50%;
 	right: 3px;
+	display: inline-block;
 	background: #E2F4FD;
 	border-radius: 2px;
 	color: #0D77AC !important;
@@ -12,17 +13,12 @@ a#n-sidebar-discussionboard-new {
 	text-align: center;
 	text-shadow: none;
 	transition: all .1s ease-in;
+	transform: translateY(-50%);
 	width: 15px;
 	height: 15px;
+	padding: 0;
 	overflow: hidden;
 	user-select: none;
-}
-a#n-sidebar-discussionboard-new:hover {
-	background: #1AA7EE;
-	color: #FFF !important;
-	text-decoration: none;
-	transition: all .1s ease-in;
-	width: 5.5em;
 }
 a#n-sidebar-discussionboard-new:hover::before {
 	content: '${wgULS('添加话题', '添加話題')}';
@@ -35,18 +31,28 @@ a#n-sidebar-discussionboard-new:focus {
 	text-decoration: none;
 }
 `);
+const NEW_SKIN = mw.config.get('skin') == 'vector-2022' ? true : false;
 const discussionBoard = $('#n-sidebar-discussionboard').clone();
 const discussionBoardNames = [wgULS('技术实现', '技術實現'), wgULS('权限变更', '權限變更'), wgULS('操作申请', '操作申請'), wgULS('方针政策', '方針政策'), wgULS('页面相关', '頁面相關'), wgULS('提问求助', '提問求助'), wgULS('群组信息', '群組資訊')];
 const discussionBoardNamesRaw = ['技术实现', '权限变更', '操作申请', '方针政策', '页面相关', '提问求助', '群组信息'];
 const discussionBoardAbbrs = ['tec', 'rgt', 'act', 'pol', 'pge', 'que', 'grp'];
-const discussionBoardPortalObj = $('<div>').attr({
+const discussionBoardPortalObj = NEW_SKIN
+	? $('<div>').attr({
+		'id': 'p-discussionboard',
+		'class': 'vector-menu mw-portlet mw-portlet-discussionboard',
+		'lang': mw.config.get('wgUserLanguage'),
+		'dir': 'ltr'
+		})
+		.append($('<div>').addClass('vector-menu-heading').text(wgULS('讨论版', '討論版')))
+		.append($('<div>').addClass('vector-menu-content').append($('<ul>').addClass('vector-menu-content-list')))
+	: $('<div>').attr({
 		'id': 'p-discussionboard',
 		'class': 'portal',
 		'role': 'navigation',
 		'aria-labelledby': 'p-discussionboard-label'
-	})
-	.append($('<h3>').attr('id', 'p-discussionboard-label').text(wgULS('讨论版', '討論版')))
-	.append($('<div>').addClass('body').append($('<ul>')));
+		})
+		.append($('<h3>').attr('id', 'p-discussionboard-label').text(wgULS('讨论版', '討論版')))
+		.append($('<div>').addClass('body').append($('<ul>')));
 $('#p-navigation').after(discussionBoardPortalObj);
 $('#n-sidebar-discussionboard').hide();
 for (let i = 0; i < discussionBoardNames.length; i++) {
@@ -58,8 +64,10 @@ for (let i = 0; i < discussionBoardNames.length; i++) {
 			'rel': 'noopener noreferrer',
 			'id': 'n-sidebar-discussionboard-new'
 		});
-	const discussionBoardLiObj = $('<li>').attr('id', 'n-sidebar-discussionboard' + discussionBoardAbbrs[i])
-		.append($('<a>').attr('href', '/萌娘百科_talk:讨论版/' + discussionBoardNamesRaw[i]).text(discussionBoardNames[i]))
+	let discussionBoardLiObj = NEW_SKIN
+		? $('<li>').attr('id', 'n-sidebar-discussionboard' + discussionBoardAbbrs[i]).addClass('mw-list-item')
+		: $('<li>').attr('id', 'n-sidebar-discussionboard' + discussionBoardAbbrs[i]);
+	discussionBoardLiObj.append($('<a>').attr('href', '/萌娘百科_talk:讨论版/' + discussionBoardNamesRaw[i]).text(discussionBoardNames[i]))
 		.append(discussionBoardNewBtn)
 		.css('position', 'relative');
 	$('#p-discussionboard ul').append(discussionBoardLiObj);
