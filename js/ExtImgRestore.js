353a4e8df18bbf4dcb05f1985ace3c311ca331a4
Saki Itomi <gpc2843661009@gmail.com>
2025-10-11 18:08:28 +0800

    Update ExtImgRestore.js
    - Adapt to display adjustment of external images.

--- js/ExtImgRestore.js
+++ js/ExtImgRestore.js
@@ -1,7 +1,7 @@
 $(() => {
 	if (mw.config.get("wgNamespaceNumber") != -1
 		&& !["submit", "edit"].includes(mw.config.get("wgAction"))
-		&& $('.moe-img-error').length > 0) {
+		&& $('.moe-img-blocked').length > 0) {
 
 		mw.loader.addStyleTag(`
 		.pc-img-restored {
@@ -20,28 +20,10 @@ $(() => {
 			max-width: 100%;
 			height: auto;
 		}
-
-		span.error.moe-img-error {
-			background: var(--background-color-disabled-subtle, rgb(var(--theme-card-background-color--rgb)));
-			border-radius: .2em;
-			color: var(--color-base, rgb(var(--theme-text-color--rgb))) !important;
-			font-size: small !important;
-			padding: .1em .3em;
-		}
-
-		span.error.moe-img-error::before {
-			content: '${wgULS('▼此外部图像被站点阻止显示，因', '▼此外部圖像被站點阻止顯示，因')}';
-		}
-
-		span.error.moe-img-error > code {
-			background: none;
-			border: none;
-			padding: 0;
-		}
 		`);
 
-		$('.moe-img-error').each(function() {
-			const src = $(this).attr('data-src-input');
+		$('.moe-img-blocked').each(function() {
+			const src = $(this).attr('href');
 			const $this = $(this);
 			const img = new Image();
 
