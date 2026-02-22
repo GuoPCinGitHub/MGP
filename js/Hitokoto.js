b86e416d405b07b39b29b7f8d1dd3410ef83a13c
Saki Itomi <gpc2843661009@gmail.com>
2026-02-22 11:17:50 +0800

    Update Hitokoto.js
    - Add titles for links.
    - Clean styles.

--- js/Hitokoto.js
+++ js/Hitokoto.js
@@ -43,10 +43,6 @@ mw.loader.addStyleTag(`
 	from { transform: rotate(0deg); }
 	to { transform: rotate(360deg); }
 }
-
-#hitokoto_refresh.loading svg {
-	display: block;
-}
 `);
 
 $(() => {
@@ -61,8 +57,8 @@ $(() => {
 	const refreshIcon = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><path d="M5.46257 4.43262C7.21556 2.91688 9.5007 2 12 2C17.5228 2 22 6.47715 22 12C22 14.1361 21.3302 16.1158 20.1892 17.7406L17 12H20C20 7.58172 16.4183 4 12 4C9.84982 4 7.89777 4.84827 6.46023 6.22842L5.46257 4.43262ZM18.5374 19.5674C16.7844 21.0831 14.4993 22 12 22C6.47715 22 2 17.5228 2 12C2 9.86386 2.66979 7.88416 3.8108 6.25944L7 12H4C4 16.4183 7.58172 20 12 20C14.1502 20 16.1022 19.1517 17.5398 17.7716L18.5374 19.5674Z"></path></svg>`;
 
 	let container = $('<div>').attr('id', 'hitokoto').append(
-		$('<a>').attr({id: 'hitokoto_logo', target: '_blank', href: 'https://hitokoto.cn'}).text('㋪'),
-		$('<a>').attr({id: 'hitokoto_text', target: '_blank'}).text('正在获取一言……'),
+		$('<a>').attr({id: 'hitokoto_logo', target: '_blank', title: '一言', href: 'https://hitokoto.cn'}).text('㋪'),
+		$('<a>').attr({id: 'hitokoto_text', target: '_blank', title: '在一言查看'}).text('正在获取一言……'),
 		$('<span>').attr({id: 'hitokoto_refresh', title: '换一个'}).html(refreshIcon)
 	);
 
